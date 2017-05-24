b9195bdca032f8c03514d7d5c1fbb7025dce5fb5
facumila <facundomm3@gmail.com>
2017-05-23 23:42:03 -0300

    contacto styles

--- contacto.php
+++ contacto.php
@@ -4,19 +4,36 @@ $titulo="Contacto";
 $contacto=true;
 require_once __PATH_INC__."header.inc.php";
 ?>
-<h1 class="naranja">INFORMACIÓN DE CONTACTO</h1>
-<br /><br />
-Si querés comunicarte, podés hacerlo a través de:
-<hr style="border:1px dotted #DB4D1B;" />
-<br />
-<table class="contacto" border="0" cellspacing="0" cellpadding="0">
-<tr><td height="44"><img src="img/icoTel.jpg" width="40" height="40" border="0"/></td><td>(0341) 447-2395 / 447-2396</td></tr>
-<tr><td height="44"><img src="img/icoSms.jpg" alt=" " width="40" height="40" border="0"/><br /></td><td>(0341) (15) 388-6677</td></tr>
-<tr><td height="44"><a href="mailto:deushuaiaalaquiaca@yahoo.com.ar"><img src="img/icoMail.jpg" alt="" width="40" height="40" border="0"/></a></td><td><a href="mailto:deushuaiaalaquiaca@yahoo.com.ar">deushuaiaalaquiaca@yahoo.com.ar</a></td></tr>
-<tr><td height="44"><a href="https://www.facebook.com/profile.php?id=100000182077268 " target="_blank"><img src="img/icoFB.jpg" alt="" width="40" height="40" border="0"/></a></td><td><a href="https://www.facebook.com/profile.php?id=100000182077268" target="_blank">La Canción del País</a></td></tr>
-<tr><td height="44"><a href="http://twitter.com/dualaqradio" target="_blank"><img src="img/icoTW.jpg" alt="" width="40" height="40" border="0"/></a></td><td> <a href="https://twitter.com/canciondelpais" target="_blank">@canciondelpais</a></td></tr>
-</table>
-<br /><br /><br /><br /><br />
-<?php
-require_once __PATH_INC__."/footer.inc.php";
-?>
+<div class="container">
+	<div class="seccion-head">INFORMACIÃ“N DE CONTACTO</div>
+</div>
+<div class="container" style="max-width:1098px">
+<div class="contact-info">Si querÃ©s comunicarte, podÃ©s hacerlo a travÃ©s de:
+<ul>
+  <li><i class="fa fa-phone-square" aria-hidden="true"></i> (0341) 447-2395 / 447-2396</li>
+  <li><i class="fa fa-paper-plane" aria-hidden="true"></i> (0341) (15) 388-6677</li>
+  <li><i class="fa fa-envelope" aria-hidden="true"></i><a href="mailto:deushuaiaalaquiaca@yahoo.com.ar"> deushuaiaalaquiaca@yahoo.com.ar</a></li>
+  <li><i class="fa fa-facebook-square" aria-hidden="true"></i><a href="https://www.facebook.com/profile.php?id=100000182077268" target="_blank"> La CanciÃ³n del PaÃ­s</a></li>
+  <li><i class="fa fa-twitter-square" aria-hidden="true"></i><a href="https://twitter.com/canciondelpais" target="_blank"> @canciondelpais</a></li>
+</ul>
+</div>
+</div>
+<!-- componente footer -->
+<div class="container-fluid" style="background-color: #eee">
+<div class="container" style="width:80%">
+	<div class="clear-big"></div>
+	<div class="container-fluid">
+	<?php
+		include 'components-ui/header-footer.php';
+	?>
+	<div class="clear-big"></div>
+	<div class="row">
+        <div class="col-md-1"></div>
+        <div class="col-md-2"><span style="font-size: 12px; color: #265d8c;">@ La Canciï¿½n del Paï¿½s 2017</span></div>
+        <div class="col-md-8"></div>
+        <div class="col-md-1"></div>
+    </div>
+    </div>
+	<div class="clear-big"></div>
+	</div>
+</div>
