ce2fd875fb876b076ef5940d8356f2019952a128
facundomila <facundomm3@gmail.com>
2017-05-24 11:31:27 -0500

    nota updates

--- nota.php
+++ nota.php
@@ -1,121 +1,3 @@
-<style>
-.nota-img-head {
-	width: 100%;
-	height: 350px;
-	overflow: hidden;
-	position: relative;
-}
-.nota-img-head img {
-	width: 100%;
-}
-.nota-img-head .overlay {
-	background-color: #265d8c;
-	opacity: 0.3;
-	width: 100%;
-	height: 100%;
-}
-.logo-nota {
-	margin: 35px 50%;
-	position: relative;
-	left:-50px;
-}
-.fecha-larga {
-	color: #265d8c;
-	font-weight: bolder;
-	margin: auto;
-	width: 300px;
-	text-align: center;
-}
-.compartir h1 {
-	color: #265d8c;
-	font-weight: bolder;
-	margin: 15px auto 35px;
-	width: 500px;
-	text-align: center;
-	line-height: 24px;
-	font-family: 'Titillium Web', sans-serif !important;
-	font-size: 22px;
-}
-.nota h1 {
-	color: #265d8c;
-	font-weight: bolder;
-	margin: 15px auto 35px;
-	width: 500px;
-	text-align: center;
-	line-height: 24px;
-	font-family: 'Titillium Web', sans-serif !important;
-	font-size: 22px;
-}
-div.nota a:hover{
-    color: red;
-    text-decoration: none;
-}
-.content {
-	margin: 0;
-	width: 100%;
-}
-.content p{
-	color: #265d8c;
-	width: 80%;
-	margin: 20px auto;
-}
-.content img.left{
-	width: 90% !important;
-	margin-right: 10%;
-}
-.content img.right{
-	width: 90% !important;
-	margin-left: 10%;
-}
-.show-comments-box {
-	width: 80%;
-	margin: 0 10%;
-}
-.show-comments-box .title{
-	color: #265d8c;
-    font-size: 24px;
-}
-.show-comments-box .each-comment{
-	color: #333;
-    font-size: 14px;
-    background-color: #eee;
-}
-.show-comments-box .fecha{
-	color: #333;
-    font-size: 12px;
-}
-.show-comments-box .nombre{
-	color: green;
-    font-size: 12px;
-}
-.show-comments-box .comentario{
-	color: red;
-    font-size: 12px;
-}
-.nota-comment-box {
-    width: 100%;
-}
-.nota-comment-box .title{
-    color: #265d8c;
-    font-size: 24px;
-}
-.nota-comment-title input{
-    color: #333;
-    font-size: 16px;
-    padding: 8px;
-}
-.cabezal-parrafo{
-		color: #265d8c;
-  	font-size: 16px;
-  	padding: 8px;
-		letter-spacing: 5px;
-		font-weight: bolder;
-		border-bottom: 2px solid red;
-		width: 80%;
-		margin: 10%;
-}
-</style>
-
 <?php
 require_once "sistema.inc.php";
 require_once __PATH_FUN__."tiempo.fun.php";
@@ -183,7 +65,6 @@ if($Rc["provinciaID"]){
 		mysql_free_result($C3);
 	}
 
-
 $titulo=$Rc["titulo"];
 $ogTitulo=textoContent($Rc["titulo"]);
 $descripcion=textoContent("La Canci�n del Pa�s. ".$Rc["bajada"]);
@@ -194,16 +75,15 @@ require_once __PATH_INC__."header.inc.php";
 
 mysql_query("UPDATE posts SET contador=contador+1 WHERE id=".$Rc["Id"]);
 
-echo "<div class=\"nota-img-head\"><img src=\"../sitio/jpg/".$Rc["img_1"]."\"><div class=\"overlay\"></div></div>";
-echo "<div class=\"logo-nota\"><img src=\"img/lcpd_conefecto_sinfondo.png\" style=\"width:100px;height:100px\"></div>";
-echo "<div class=\"fecha-larga\">".fechaLarga($Rc["fecha"])."<span class=\"notasSeccion\"> <br> <a href=\"notas/".$Rs["link"]."/\">".$Rs["seccion"]."</a>".($Rsub["subseccion"] ? " &raquo; <a href=\"notas/".$Rs["link"]."/".$Rsub["link"]."\">".$Rsub["subseccion"]."</a>" : "")."<span class=\"notasSeccion\">$enlaceProvincia$enlaceCiudad</span></span></div>\n";
+echo "<div class=\"nota-img-head\"><img src=\"../sitio/jpg/".$Rc["img_1"]."\"></div>";
+echo "<div class=\"logo-nota\"><img src=\"img/lcpd_conefecto_sinfondo.png\" style=\"width:84px;height:84px\"></div>";
 echo "<div class=\"nota\"><h1>".$Rc["titulo"].($Rc["artista"] ? " � ".$Rc["artista"] : "").($_GET["preview"]=="-preview" ? " [preview]" : "")."</h1></div>\n";
+echo "<div class=\"seccion-nota\">Audiovisuales</div>\n";
+echo "<div class=\"compartir\">";
+echo "<a target=\"_blank\" href=\"http://twitter.com/intent/tweet?text=".textoContent($Rc["titulo"], 115)." ".__BASE__."notas/".$Rs["link"]."/".$Rsub["link"].$Rc["link"].".html\" class=\"twitter\"><i class=\"fa fa-twitter-square\" aria-hidden=\"true\"></i></a>";
+echo "<a target=\"_blank\" href=\"http://www.facebook.com/sharer.php?u=".__BASE__."notas/".$Rs["link"]."/".$Rsub["link"].$Rc["link"].".html\"><i class=\"fa fa-facebook\" aria-hidden=\"true\"></i></a></div>";
+echo "<div class=\"fecha-nota\">".fechaLarga($Rc["fecha"])."</div>\n";
 echo "<div class=\"content\">".trim($Rc["cuerpo"])."</div>\n";
-
-echo "<div class=\"compartir\"><h1>Te gustó la nota? Compartila!\n";
-echo "<a target=\"_blank\" href=\"http://www.facebook.com/sharer.php?u=".__BASE__."notas/".$Rs["link"]."/".$Rsub["link"].$Rc["link"].".html\"><img src=\"img/icoFBsmall.png\" title=\"compartir en Facebook\" alt=\"compartir en Facebook\"></a> ";
-echo "<a target=\"_blank\" href=\"http://twitter.com/intent/tweet?text=".textoContent($Rc["titulo"], 115)." ".__BASE__."notas/".$Rs["link"]."/".$Rsub["link"].$Rc["link"].".html\" class=\"twitter\"><img src=\"img/icoTWsmall.png\" title=\"compartir en Twitter\" alt=\"compartir en Twitter\"></a> ";
-echo "<g:plusone size=\"small\" annotation=\"none\" href=\"".__BASE__."notas/".$Rs["link"]."/".$Rsub["link"].$Rc["link"].".html\"></g:plusone></div>\n";
 ?>
 
 <div class="cabezal-parrafo">
